2d32be140543b5d6bfabc9f835c4b82a3bd1cdba
pintoinfant <pintoinfant5650@gmail.com>
2025-09-27 13:03:00 +0530

    Add Next.js config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["static.usernames.app-backend.toolsforhumanity.com"],
+  },
+  allowedDevOrigins: ["*"], // Add your dev origin here
+  reactStrictMode: false,
+};
+
+export default nextConfig;
